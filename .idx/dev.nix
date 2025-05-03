405192a8885e531175bbb7d4b0b9a2e1918862aa
parth <parth.rk.vasave@gmail.com>
2025-05-03 14:37:04 +0000

    initial commit

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,18 +1,12 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://firebase.google.com/docs/studio/customize-workspace
+
 {pkgs}: {
-  # Which nixpkgs channel to use.
-  channel = "stable-24.11"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
+  channel = "stable-24.11"; 
   packages = [
     pkgs.nodejs_20
   ];
-  # Sets environment variables in the workspace
   env = {};
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
     ];
     workspace = {
       onCreate = {
@@ -21,7 +15,6 @@
         ];
       };
     };
-    # Enable previews and customize configuration
     previews = {
       enable = true;
       previews = {
